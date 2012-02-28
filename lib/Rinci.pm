ac2e6e7f39e2f268988349a77d8f3d3248aa0b7e
Steven Haryanto <stevenharyanto@gmail.com>
2012-02-28 01:30:03 +0000

    Import of SHARYANTO/Rinci-1.1.8 from CPAN.
    gitpan-cpan-distribution: Rinci
    gitpan-cpan-version:      1.1.8
    gitpan-cpan-path:         SHARYANTO/Rinci-1.1.8.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Rinci.pm
+++ lib/Rinci.pm
@@ -1,6 +1,6 @@
 package Rinci;
 
-our $VERSION = '1.1.7'; # VERSION
+our $VERSION = '1.1.8'; # VERSION
 
 1;
 # ABSTRACT: Language-neutral metadata for your code
@@ -14,7 +14,7 @@ Rinci - Language-neutral metadata for your code
 
 =head1 VERSION
 
-version 1.1.7
+version 1.1.8
 
 =head1 AUTHOR
 
--- lib/Rinci/Schema.pm
+++ lib/Rinci/Schema.pm
@@ -134,7 +134,7 @@ Rinci::Schema - Schemas for Rinci metadata
 
 =head1 VERSION
 
-version 1.1.7
+version 1.1.8
 
 =head1 AUTHOR
 
