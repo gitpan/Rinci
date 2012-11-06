d97f622cadb2f4373c612640dd3ca518d1b4fe65
Steven Haryanto <stevenharyanto@gmail.com>
2012-11-06 15:46:55 +0000

    Import of SHARYANTO/Rinci-1.1.32 from CPAN.
    gitpan-cpan-distribution: Rinci
    gitpan-cpan-version:      1.1.32
    gitpan-cpan-path:         SHARYANTO/Rinci-1.1.32.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Rinci/Schema.pm
+++ lib/Rinci/Schema.pm
@@ -4,7 +4,7 @@ use 5.010;
 use strict;
 use warnings;
 
-our $VERSION = '1.1.31'; # VERSION
+our $VERSION = '1.1.32'; # VERSION
 
 # only validates metadata v1.1
 
@@ -137,7 +137,7 @@ Rinci::Schema - Schemas for Rinci metadata
 
 =head1 VERSION
 
-version 1.1.31
+version 1.1.32
 
 =head1 AUTHOR
 
