7150aacbc5c778b7c458d45587cffecda90a339e
Steven Haryanto <stevenharyanto@gmail.com>
2013-04-11 12:42:10 +0000

    Import of SHARYANTO/Rinci-1.1.35 from CPAN.
    gitpan-cpan-distribution: Rinci
    gitpan-cpan-version:      1.1.35
    gitpan-cpan-path:         SHARYANTO/Rinci-1.1.35.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Rinci.pm
+++ lib/Rinci.pm
@@ -1,6 +1,6 @@
 package Rinci;
 
-our $VERSION = '1.1.34'; # VERSION
+our $VERSION = '1.1.35'; # VERSION
 
 1;
 # ABSTRACT: Language-neutral metadata for your code
@@ -8,13 +8,15 @@ our $VERSION = '1.1.34'; # VERSION
 __END__
 =pod
 
+=encoding utf-8
+
 =head1 NAME
 
 Rinci - Language-neutral metadata for your code
 
 =head1 VERSION
 
-version 1.1.34
+version 1.1.35
 
 =head1 AUTHOR
 
@@ -22,10 +24,17 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2012 by Steven Haryanto.
+This software is copyright (c) 2013 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+
+None are exported by default, but they are exportable.
 
 =cut
 
