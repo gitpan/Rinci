ca2f281a12f409bbd513dd45f66bab75d53d86ae
Steven Haryanto <stevenharyanto@gmail.com>
2014-04-27 08:06:11 +0000

    Import of SHARYANTO/Rinci-1.1.44 from CPAN.
    gitpan-cpan-distribution: Rinci
    gitpan-cpan-version:      1.1.44
    gitpan-cpan-path:         SHARYANTO/Rinci-1.1.44.tar.gz
    gitpan-cpan-author:       SHARYANTO
    gitpan-cpan-maturity:     released

--- lib/Rinci.pm
+++ lib/Rinci.pm
@@ -1,6 +1,6 @@
 package Rinci;
 
-our $VERSION = '1.1.43'; # VERSION
+our $VERSION = '1.1.44'; # VERSION
 
 1;
 # ABSTRACT: Language-neutral metadata for your code
@@ -17,7 +17,11 @@ Rinci - Language-neutral metadata for your code
 
 =head1 VERSION
 
-version 1.1.43
+version 1.1.44
+
+=head1 RELEASE DATE
+
+2014-04-27
 
 =head1 HOMEPAGE
 
@@ -41,7 +45,7 @@ Steven Haryanto <stevenharyanto@gmail.com>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2013 by Steven Haryanto.
+This software is copyright (c) 2014 by Steven Haryanto.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
---
+++ lib/Sah/Schema/Rinci.pm
@@ -0,0 +1,190 @@
+package Sah::Schema::Rinci;
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $VERSION = '1.1.44'; # VERSION
+our $DATE = '2014-04-27'; # DATE
+
+our %SCHEMAS;
+
+$SCHEMAS{rinci} = [hash => {
+    # tmp
+    _prop => {
+        # from defhash
+        v => {},
+        defhash_v => {},
+        name => {},
+        summary => {},
+        description => {},
+        tags => {},
+        default_lang => {},
+        x => {},
+
+        entity_v => {},
+        links => {
+            _elem_prop => {
+                # XXX include from defhash
+
+                uri => {},
+                title => {},
+            },
+        },
+    },
+}];
+
+$SCHEMAS{rinci_function} = [hash => {
+    # tmp
+    _prop => {
+        # from defhash
+        v => {},
+        defhash_v => {},
+        name => {},
+        summary => {},
+        description => {},
+        tags => {},
+        default_lang => {},
+        x => {},
+
+        # from common rinci metadata
+        entity_v => {},
+        links => {},
+
+        is_func => {},
+        is_meth => {},
+        is_class_meth => {},
+        args => {
+            _elem_prop => {
+                # from defhash
+                summary => {},
+                description => {},
+                tags => {},
+
+                schema => {},
+                default => {},
+                req => {},
+                pos => {},
+                greedy => {},
+                cmdline_aliases => {},
+                cmdline_on_getopt => {},
+                completion => {},
+                element_completion => {},
+                cmdline_src => {},
+            },
+        },
+        args_as => {},
+        result => {
+            _prop => {
+                # from defhash
+                summary => {},
+                description => {},
+
+                schema => {},
+                statuses => {
+                    _val_prop => {
+                        # from defhash
+                        summary => {},
+                        description => {},
+
+                        schema => {},
+                    },
+                },
+            },
+        },
+        result_naked => {},
+        examples => {
+            _elem_prop => {
+                # from defhash
+                summary => {},
+                description => {},
+                tags => {},
+
+                args => {},
+                argv => {},
+                src => {},
+                src_plang => {},
+                status => {},
+                result => {},
+                test => {},
+            },
+        },
+        features => {
+            _prop => {
+                reverse => {},
+                tx => {},
+                dry_run => {},
+                pure => {},
+                immutable => {},
+                idempotent => {},
+            },
+        },
+        deps => {
+            _prop => {
+                all => {},
+                any => {},
+                none => {},
+                env => {},
+                prog => {},
+                code => {},
+                tmp_dir => {},
+                trash_dir => {},
+            },
+        },
+    },
+}];
+
+# rinci_package
+# rinci_variable
+# rinci_result
+
+
+1;
+# ABSTRACT: Sah schemas for Rinci metadata
+
+__END__
+
+=pod
+
+=encoding UTF-8
+
+=head1 NAME
+
+Sah::Schema::Rinci - Sah schemas for Rinci metadata
+
+=head1 VERSION
+
+version 1.1.44
+
+=head1 RELEASE DATE
+
+2014-04-27
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Rinci>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Rinci>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Rinci>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
+
+=head1 AUTHOR
+
+Steven Haryanto <stevenharyanto@gmail.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Steven Haryanto.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
